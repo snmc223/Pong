4c7f300fb3941af85f9273d20dc946a96100839b
Nicole <snmcgovern17@gmail.com>
2021-11-09 07:15:27 -0500

    changes made to pong game

--- pong.js
+++ pong.js
@@ -13,6 +13,12 @@ var topSpeedOfBall = 10;
 var leftSpeedOfBall = 0;
 var score1 = 0;
 var score2 = 0;
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
 // 2 players
 
@@ -78,4 +84,23 @@ window.setInterval(function show() {
     document.getElementById('paddle2').style.top = positionOfPaddle2 + 'px';
 
 }, 1000/60)
+
+
+
+// function drawBall() {
+//     ctx.beginPath();
+//     ctx.arc(x, y, 10, 0, Math.PI*2);
+//     ctx.fillStyle = "#0095DD";
+//     ctx.fill();
+//     ctx.closePath();
+// }
+
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     drawBall();
+//     x += dx;
+//     y += dy;
+// }
+
+// setInterval(draw, 10);
 
